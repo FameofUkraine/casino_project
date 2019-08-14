b0f6236a7fcad9ca0a53d6b520d5542567810966
FameofUkraine <7295016@gmail.com>
2019-08-14 20:48:18 +0300

    Casino blog project - -  6th commit (finished categories)

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,7 +2,10 @@
 
 namespace app\controllers;
 
+use app\models\Article;
+use app\models\Category;
 use Yii;
+use yii\data\Pagination;
 use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\Response;
@@ -49,7 +52,7 @@ class SiteController extends Controller
             ],
             'captcha' => [
                 'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testMe' : null,
             ],
         ];
     }
@@ -61,7 +64,46 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        // build a DB query to get all articles
+        $query = Article::find();
+//       echo '<pre>';
+//        var_dump($query);die();
+//echo '</pre>';
+        // get the total number of articles (but do not fetch the article data yet)
+        $count = $query->count();
+
+        // create a pagination object with the total count
+        $pagination = new Pagination(['totalCount' => $count, 'pageSize' => 1]);
+
+        // limit the query using the pagination and retrieve the articles
+        $articles = $query->offset($pagination->offset)
+                            ->limit($pagination->limit)
+                            ->all();
+
+        $popular = Article::find()->orderBy('viewed desc')->limit(3)->all();
+        $recent = Article::find()->orderBy('date asc')->limit(4)->all();
+        $categories = Category::find()->all();
+
+        return $this->render('index', [
+            'articles' => $articles,
+            'pagination' => $pagination,
+            'popular' => $popular,
+            'recent' => $recent,
+            'categories' => $categories
+        ]);
+
+
+    }
+
+    public function actionView()
+    {
+    return $this->render('single');
+
+    }
+
+    public function actionCategory()
+    {
+    return $this->render('category');
     }
 
     /**
